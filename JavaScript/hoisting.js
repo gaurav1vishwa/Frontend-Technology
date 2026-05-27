560bfeec1a11888b36df6f24cbbe7605d073f727
Gaurav Vishwakarma <shnivishwakarma1234@gmail.com>
2026-05-27 01:45:11 -0700

    hoisting

---
+++ JavaScript/hoisting.js
@@ -0,0 +1,23 @@
+
+
+console.log(a);
+
+let a =29;
+
+
+hoisting(23);
+
+function hoisting(a) {
+    console.log(a);
+    
+}
+
+          // MEMORY MANAGEMENT  AND HOW THE MEMORY WILL BE ALLOCATGED AND USED
+
+
+          // arraow function is never hoisted but function declaration is hoisted and variable declaration is also hoisted but not the initialization
+
+          // in case of var the variable declaration is hoisted but not the initialization and in case of let and const the variable declaration is also hoisted but it is in temporal dead zone until it is initialized
+
+          
+
